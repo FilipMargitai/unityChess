535a2c979da7c437bd1d7fd18e97bf86e542e4fe
FilipMargitai <filip.margitai@pslib.cz>
2020-11-03 18:59:41 +0100

    starting position

---
+++ Chess/Assets/Scripts/Info.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Info : MonoBehaviour
+{
+    public GameObject startingPosition;
+    void Start()
+    {
+        var p = startingPosition.transform.position;
+        transform.position = new Vector3(p.x, p.y + 0.38f, p.z);
+    }
+}
