c61f33bc7e3abc9fa622252b01882bbcbd83d5be
FilipMargitai <filip.margitai@pslib.cz>
2020-11-03 19:37:22 +0100

    Wrong execution, let's use 2D array next time

---
+++ Chess/Assets/Scripts/pieceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class pieceInfo : MonoBehaviour
+{
+    public GameObject startingPosition;
+    //[HideInInspector]
+    public GameObject currentPosition;
+    public pieceType pieceName;
+    void Start()
+    {
+        currentPosition = startingPosition;
+
+        var p = startingPosition.transform.position;
+        transform.position = new Vector3(p.x, p.y + 0.38f, p.z);
+    }
+}
---
+++ Chess/Assets/Scripts/piecesUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class piecesUpdate : MonoBehaviour
+{
+    public GameObject Board;
+    void Update()
+    {
+        foreach(Transform child in transform)
+        {
+            
+        }
+    }
+}
